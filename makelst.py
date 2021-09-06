6bf72131781437017663b21ae23f657b6781ce21
Seeky <dan.hally@gmail.com>
2021-09-06 19:31:05 +0100

    Automatic lst building

---
+++ makelst.py
@@ -0,0 +1,19 @@
+from os import system
+
+SPMPC = "C:/Users/Daniel/spm-practice-codes"
+BASE_PATH = SPMPC + "/include/spm.eu0.lst"
+MOD_PATH = SPMPC + "/spm-practice-codes.eu0.elf"
+TEMP_PATH = "spmpc.lst"
+OUT_PATH = "include/spm.eu0.lst"
+
+system(f"%ELF2LST% {MOD_PATH} {TEMP_PATH}")
+
+with open(BASE_PATH) as f:
+    baseLst = f.read()
+
+with open(TEMP_PATH) as f:
+    modLst = f.read()
+
+with open(OUT_PATH, 'w') as f:
+    f.write(baseLst + '\n\n' + modLst)
+
