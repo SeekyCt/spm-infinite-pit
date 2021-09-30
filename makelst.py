efce9efe45d1bd1935ffdc1ae2d3104e6e5c6897
Seeky <dan.hally@gmail.com>
2021-09-30 21:04:47 +0100

    Lst update

--- makelst.py
+++ makelst.py
@@ -3,10 +3,13 @@ from os import system
 SPMPC = "C:/Users/Daniel/spm-practice-codes"
 BASE_PATH = SPMPC + "/include/spm.eu0.lst"
 MOD_PATH = SPMPC + "/spm-practice-codes.eu0.elf"
+MOD_REL_PATH = SPMPC + "/spm-practice-codes.eu0.rel"
 TEMP_PATH = "spmpc.lst"
 OUT_PATH = "include/spm.eu0.lst"
 
 system(f"%ELF2LST% {MOD_PATH} {TEMP_PATH}")
+
+system(f"cat {MOD_REL_PATH} > host.rel")
 
 with open(BASE_PATH) as f:
     baseLst = f.read()
